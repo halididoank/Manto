7e4e12ea93e4339b28e1a1a012ff1ada85176b41
halididoank <halididoank@gmail.com>
2023-07-07 09:58:05 +0800

    fet: slice movie detail

--- resources/js/Components/FeaturedMovie.js
+++ resources/js/Components/FeaturedMovie.js
@@ -1,4 +1,6 @@
 import PropType from 'prop-types'
+import { Link } from '@inertiajs/inertia-react'
+
 FeaturedMovie.propTypes = {
     slug: PropType.string.isRequired,
     name: PropType.string.isRequired,
@@ -45,7 +47,7 @@ export default function FeaturedMovie({
                     <img src="/icons/ic_play.svg" width="50" alt="" />
                 </div>
             </div>
-            <a href={slug} className="inset-0 absolute z-50"></a>
+            <Link href={route("prototype.movie.show", slug)} className="inset-0 absolute z-50"></Link>
         </div>
     );
 }
--- resources/js/Components/MovieCard.js
+++ resources/js/Components/MovieCard.js
@@ -1,16 +1,13 @@
-import PropType from 'prop-types'
+import PropType from "prop-types";
+import { Link } from "@inertiajs/inertia-react";
+
 MovieCard.propType = {
-    slug:PropType.string.isRequired,
+    slug: PropType.string.isRequired,
     name: PropType.string.isRequired,
     category: PropType.string.isRequired,
     thumbnail: PropType.string.isRequired,
-}
-export default function MovieCard({
-    slug,
-    name,
-    category,
-    thumbnail,
-}) {
+};
+export default function MovieCard({ slug, name, category, thumbnail }) {
     return (
         <div className="absolute group overflow-hidden mr-[30px]">
             <img
@@ -20,9 +17,7 @@ export default function MovieCard({
             />
             <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px] rounded-br-[28px]">
                 <div className="px-7 pb-7">
-                    <div className="font-medium text-xl text-white">
-                        {name}
-                    </div>
+                    <div className="font-medium text-xl text-white">{name}</div>
                     <p className="mb-0 text-gray-300 text-base mt-[10px]">
                         {category}
                     </p>
@@ -34,7 +29,10 @@ export default function MovieCard({
             >
                 <img src="/icons/ic_play.svg" className="" width="50" alt="" />
             </div>
-            <a href={slug} className="inset-0 absolute z-50"></a>
+            <Link
+                href={route("prototype.movie.show", slug)}
+                className="inset-0 absolute z-50"
+            ></Link>
         </div>
     );
 }
